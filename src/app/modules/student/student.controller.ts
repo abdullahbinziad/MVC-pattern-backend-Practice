c420470bb516255910ed39138c9d2c8cdf5a79da
Flex-Softr <abdullahbinziad@gmail.com>
2023-11-20 01:13:31 +0600

    now start validation

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,16 +1,16 @@
-import { Request, Response } from 'express';
-import { StudentServices } from './student.service';
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const student = req.body;
+    const { student: studentData } = req.body;
 
     //will call service func to send this data
-    const result = await StudentServices.createStudentIntoDB(student);
+    const result = await StudentServices.createStudentIntoDB(studentData);
 
     res.status(200).json({
       success: true,
-      message: 'Student is created Successfully',
+      message: "Student is created Successfully",
       data: result,
     });
   } catch (error) {
@@ -18,6 +18,39 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudents = async (req: Request, res: Response) => {
+  const result = await StudentServices.getAllStudentsFromDB();
+
+  res.status(200).json({
+    success: true,
+    message: "Students are retrived Successfully",
+    data: result,
+  });
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//getsingle studentData
+
+const getsingleStudents = async (req: Request, res: Response) => {
+  const studentID = req.params.id;
+  const result = await StudentServices.getSingleStudentFromDB(studentID);
+
+  res.status(200).json({
+    success: true,
+    message: "Students are retrived Successfully",
+    data: result,
+  });
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudents,
+  getsingleStudents,
 };
